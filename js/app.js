5ee9878511ada00c01cda9a5a4f66a22b3f22d78
Maria Kamal <mariaafahad@gmail.com>
2021-09-14 19:20:26 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -14,17 +14,21 @@ const showProducts = (products) => {
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${product.image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <h3>Price: $ ${product.price}</h3>
+      <h5>Raiting: ${product.rating.rate}</h5>
+      <h5>Person rated: ${product.rating.count}</h5>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-info" style="border:none">add to cart</button>
+      <button id="details-btn" class="btn btn-warning">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+// above inside innerHtml tag  image+title+catagory+price=rate+pplraiting is been dynamically set
+// and also in button product id and its price is set so that it can add it values
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -32,11 +36,14 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  // calling for total price
+  updateTotal();
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
+  // parseFloat for point value addition
   return converted;
 };
 
@@ -45,12 +52,14 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);;
+  // done to.Fixed for getting up to 2 digit after point
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
+  // done to.Fixed for getting up to 2 digit after point
 };
 
 // update delivery charge and total Tax
@@ -69,12 +78,14 @@ const updateTaxAndCharge = () => {
     setInnerText("total-tax", priceConverted * 0.4);
   }
 };
+// above some condition in if set for calculating delivary charge + tax charge
 
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
+  // done to.Fixed for getting up to 2 digit after point
 };
 loadProducts();
